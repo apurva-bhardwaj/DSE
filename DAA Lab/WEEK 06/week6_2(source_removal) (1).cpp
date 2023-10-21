132c1304f7540a00b190941aa2535aeeb440d706
Apurva Bhardwaj <95472465+apurva-bhardwaj@users.noreply.github.com>
2023-10-21 22:12:18 +0530

    Add files via upload

---
+++ DAA Lab/WEEK 06/week6_2(source_removal) (1).cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAXN = 1000;
+
+bool adj[MAXN][MAXN];
+int indeg[MAXN];
+
+void topological_sort(int N) {
+    int remaining = N;
+    
+    while (remaining > 0) {
+        // Find a vertex with no incoming edges (indegree = 0)
+        int u = -1;
+        
+        for (int i = 0; i < N; i++) {
+            if (indeg[i] == 0) {
+                u = i;
+                break;
+            }
+        }
+        
+        if (u == -1) {
+            // There is a cycle in the graph, so we can't do a topological sort
+            cout << "Error: the graph contains a cycle\n";
+            return;
+        }
+        
+        // Output u and remove all outgoing edges from u
+        cout << u << " ";
+        
+        for (int v = 0; v < N; v++) {
+            if (adj[u][v]) {
+                indeg[v]--;
+            }
+        }
+        
+        indeg[u] = -1;  // Mark vertex as visited
+        remaining--;
+    }
+    cout << endl;
+}
+
+int main() {
+    int V, E;
+    
+    cout << "Enter the number of vertices and edges : ";
+    cin >> V >> E;
+    
+    cout << "Enter the edges (u v):" << endl;
+    
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = true;
+        indeg[v]++;
+    }
+    
+    topological_sort(V);
+    
+    return 0;
+}
